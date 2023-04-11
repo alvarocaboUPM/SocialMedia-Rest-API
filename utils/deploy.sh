e828d5e12bad0c36c8e829f0d3e263a41cc84ac7
alvarocabo <alvarocaboac2@gmail.com>
2023-04-11 18:52:06 +0200

    New deploy script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -4,6 +4,8 @@
 #              RUN USING SUDO               #
 
 cd ../rest-sos
-mvn clean package && echo "Build completed"
-cp ./target/rest-sos-api.war /opt/tomcat/webapps/
-sh /opt/tomcat/bin/startup.sh && echo "Tomcat running, completed successfuly"
+
+mvn clean package && echo -e "\e[36mBuild completed!\e[0m"
+
+cp ./target/rest-sos-api.war /opt/tomcat/webapps/ && echo -e "\e[36mWAR file copied\e[0m" 
+sh /opt/tomcat/bin/startup.sh && echo -e "\e[32mTomcat running, completed successfuly!\e[0m"
