8f41075ee3f20c2aab3860c6671eaa6f365bb61a
alvarocabo <alvarocaboac2@gmail.com>
2023-04-11 19:16:17 +0200

    Deploy script v2

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -7,5 +7,5 @@ cd ../rest-sos
 
 mvn clean package && echo -e "\e[36mBuild completed!\e[0m"
 
-cp ./target/rest-sos-api.war /opt/tomcat/webapps/ && echo -e "\e[36mWAR file copied\e[0m" 
+cp ./target/rest-sos-api.war /opt/tomcat/webapps/ && echo -e "\e[36mWAR file copied\e[0m"
 sh /opt/tomcat/bin/startup.sh && echo -e "\e[32mTomcat running, completed successfuly!\e[0m"
